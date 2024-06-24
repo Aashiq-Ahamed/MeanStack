6b92e61bcd2c74e5fbc56c5912feeed48959b6a0
aashiq.a <aashiq.ahamed@scicom.com.my>
2024-06-24 23:53:04 +0530

    creating front end

--- backend/database/models/list.js
+++ backend/database/models/list.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const listSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        trim:true,
-        minlength:3
+const ListSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        minlength: 3
     }
 });
 
-const list = mongoose.model('List',listSchema);
-module.exports = list;
+const List = mongoose.model('List', ListSchema);
+module.exports = List;
